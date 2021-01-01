bb3b4e579656c257e1463342a8262d9d3f64d5a7
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-12-31 20:18:16 -0800

    Initial commit

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import { Text } from 'react-native';
-import { CardSection } from './common';
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+import {CardSection} from './common';
 
-class ListItem extends Component{
-  render(){
-   const { titleStyle } = styles;
+class ListItem extends Component {
+  render() {
+    const {titleStyle} = styles;
 
-    return(
+    return (
       <CardSection>
-       <Text style={titleStyle}>{this.props.library.title}</Text>
+        <Text style={{titleStyle}}>{this.props.library.title}</Text>
       </CardSection>
     );
   }
@@ -17,8 +17,8 @@ class ListItem extends Component{
 const styles = {
   titleStyle: {
     fontSize: 18,
-    paddingLeft: 15
-  }
-}
+    paddingLeft: 15,
+  },
+};
 
 export default ListItem;
